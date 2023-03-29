15fcab715403e82a85d3c4b4631cde48ffd9816c
Ivan Gutierrez <figaron11@gmail.com>
2023-03-29 08:50:40 -0400

    Add register user

--- core/routers.py
+++ core/routers.py
@@ -1,10 +1,18 @@
 from rest_framework import routers
+
+from core.auth.viewsets.register import RegisterViewSet
 from core.user.viewsets import UserViewSet
 
 router = routers.SimpleRouter()
 
 # ##################################################################### #
 # ################### USER                       ###################### #
+# ##################################################################### #
+
+router.register(r'auth/register', RegisterViewSet, basename='auth-register')
+
+# ##################################################################### #
+# ################### AUTH                       ###################### #
 # ##################################################################### #
 
 router.register(r'user', UserViewSet, basename='user')
